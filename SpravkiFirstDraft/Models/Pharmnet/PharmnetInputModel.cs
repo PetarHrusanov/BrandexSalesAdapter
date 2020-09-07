6a7ac5ef6a59fee9a6add68ac6ba86a42f5073fd
Petar <p.hrusanov@gmail.com>
2020-09-07 16:11:15 +0300

    Pharmnet and prototype for Error Handling

---
+++ SpravkiFirstDraft/Models/Pharmnet/PharmnetInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SpravkiFirstDraft.Models.Pharmnet
+{
+    using System;
+    using Microsoft.AspNetCore.Http;
+
+    public class PharmnetInputModel
+    {
+        public string Date { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+    }
+}
